d6f1cfbca003732672826f3b5b7a1b4a91f184cf
GlebKalinichenko <gleb.kalinichenko@umbrella-web.com>
2018-02-16 00:42:25 +0300

    Implemented layout for login screen

---
+++ EyeShopping/EyeShopping/RoundButton.swift
@@ -0,0 +1,31 @@
+//
+//  RoundButton.swift
+//  EyeShopping
+//
+//  Created by Gleb Kalinichenko on 13.02.18.
+//  Copyright © 2018 Gleb Kalinichenko. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0{
+        didSet{
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0{
+        didSet{
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear{
+        didSet{
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+}
